74fd707dc328e77ea05ee9a289f97c3cea4578e4
Prince <princejaj67@gmail.com>
2024-03-17 22:20:54 +0530

    Added basic styles

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Image from 'next/image'
 const blog = () => {
   return (
     <div>
       <h1 className='container' >BLOGGGGGGGGGG PAGE</h1>
+      <div className="" style={{display : "flex",justifyContent : "center"}} >
+      <Image src='https://cdn.sanity.io/images/vr8gru94/production/e016bbd4d7d57ff27e261adf1e254d2d3c609aac-2447x849.png' style={{objectFit : "contain"}} alt='None' height={500} width={1000} />
+      </div>
     </div>
   )
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,6 @@
 "use client"
 
 import React from 'react'
-import Image from 'next/image'
 
 const contact = () => {
 
@@ -23,7 +22,7 @@ const contact = () => {
   }
 
   return (
-    <div >
+    <div>
 
 
       {/* We can use Script as here and importing them as well */}
@@ -38,12 +37,10 @@ const contact = () => {
 
       {/* <img src="https://cdn.sanity.io/images/vr8gru94/production/
       e016bbd4d7d57ff27e261adf1e254d2d3c609aac-2447x849.png" alt="None" /> */}
-      <button onClick={postt}>POST</button>
+          <div style={{display : "flex",justifyContent : "center" }}>
 
-      <div className="" style={{backgroundColor : "blue"}}>
-      <Image src='https://cdn.sanity.io/images/vr8gru94/production/e016bbd4d7d57ff27e261adf1e254d2d3c609aac-2447x849.png' style={{ backgroundColor : "red", objectFit : "contain"}} alt='None' height={500} width={1000} />
-      </div>
-
+      <button style={{fontSize : "3vh",padding : "2vh" ,margin : "2vh", borderRadius : "12vh"}}onClick={postt}>POST</button>
+        </div>
 
     </div>
   )
